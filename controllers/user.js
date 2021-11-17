58a2021bafe3cb07cc131cf851678e0212e8fcf8
Pinout <yann.pinout@hotmail.fr>
2021-11-17 15:02:50 +0100

    Modifs. Plus rien ne plante

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const db = require('../mysqlConnect');
 const dotenv = require("dotenv");
-dotenv.config({path: './.env'});
+dotenv.config({path: '../.env'});
 
 
 exports.signup = (req, res, next) => {
@@ -10,7 +10,7 @@ exports.signup = (req, res, next) => {
     const username = req.body.username;
     bcrypt.hash(password, 10)
         .then(hash => {   
-            const sqlQuery = "INSERT INTO `users` SET ?"
+            const sqlQuery = "INSERT INTO `users` SET ?" // ajoiut du nouveau user
             const bindings = {
                 username:username,
                 email:req.body.email,
@@ -20,10 +20,10 @@ exports.signup = (req, res, next) => {
             db.query(preparedUserInfo, (error, result, field) => {
                 if (error) {
                     console.log(error)
-                    return res.status(400).json("erreur")
+                    return res.status(400).json("erreur") // erreur
                 }
                 console.log('création de compte utilisateur : ' + username)
-                return res.status(201).json({message : 'Votre compte a bien été crée !'},)
+                return res.status(201).json({message : 'Votre compte a bien été crée !'},) // succès
             });
         })
         .catch(error => res.status(500).json({ error })
@@ -34,7 +34,7 @@ exports.login = (req, res, next) => {
     const password = req.body.password
     const username = req.body.username;
     db.query(
-        'SELECT * FROM users WHERE username= ?',
+        'SELECT * FROM users WHERE username= ?', // on cherche le user qui correspond à celui rentré au login
         username, 
         (error, results, _fields) => {
             console.log(results[0])
@@ -59,7 +59,6 @@ exports.login = (req, res, next) => {
 
 
 /* One connected user */
-
 exports.getConnectedUser = (req, res, next) => {
     console.log('test1')
     const token = req.headers.authorization.split(' ')[1]; 
@@ -78,8 +77,6 @@ exports.getConnectedUser = (req, res, next) => {
 
 
 /* Delete one user */
-
-
 exports.deleteUser = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1]; 
     const decodedToken = jwt.verify(token, process.env.TOKEN_USER); 
